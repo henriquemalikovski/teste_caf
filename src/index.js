175d567c477d67739dfcaf10bff19c2fe0820a8d
Henrique Malikovski <hmalikovski@gmail.com>
2024-03-25 19:32:18 -0300

    parse int

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use("/api/caf", (req, res) => {
   res.status(200).json([
     {
-      total_docs: process.env.total_docs,
-      docs_5min: process.env.docs_5min,
-      docs_10min: process.env.docs_10min,
-      docs_15min: process.env.docs_15min,
+      total_docs: parseInt(process.env.total_docs),
+      docs_5min: parseInt(process.env.docs_5min),
+      docs_10min: parseInt(process.env.docs_10min),
+      docs_15min: parseInt(process.env.docs_15min),
     },
   ]);
 });
